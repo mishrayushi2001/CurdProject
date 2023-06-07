2b4abbff9aa34d84e317c579e039cf33a88f102e
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-06-07 19:58:18 +0530

    fixig issue left

--- enroll/models.py
+++ enroll/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Users(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=70)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
--- enroll/views.py
+++ enroll/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
-
+from .forms import StudentRegistration
+from .models import User
 def add_show(request):
-    return render(request,'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm= StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm,email=em,password=pw)
+            reg.save()
+    else:
+        fm= StudentRegistration()
+    return render(request,'enroll/addandshow.html',{'form':fm}) 
